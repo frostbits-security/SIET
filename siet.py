ed744dfa0a670675ba9acfb60e0d489fcbec638c
Sab0tag3d <sabotaged@protonmail.com>
2016-11-16 19:27:53 +0300

    Update siet.py

--- siet.py
+++ siet.py
@@ -212,7 +212,7 @@ def start_tftp_serv(mode): #start fake tftp server for put config file on host
     while True:
 
         iTransfer += 1
-        print "[INFO]: Request count: %f" % iTransfer
+        print "[INFO]: Request count: %f" % int(iTransfer)
         if (iTransfer == 50):
             break
 
